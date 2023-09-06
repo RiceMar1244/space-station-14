0337a09c3afa07c41118350599a404308c400f9b
Brandon Metz <bmetz02@gmail.com>
2023-09-05 21:21:39 -0400

    Tweaked pistol recipes. Moved everything from SchematicsSystem to much more generically named StartItemConstructionOnActivateSystem.

--- Content.Shared/Construction/Components/StartItemConstructionOnActivateComponent.cs
+++ Content.Shared/Construction/Components/StartItemConstructionOnActivateComponent.cs
@@ -2,13 +2,13 @@ using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 using Content.Shared.Construction.Prototypes;
 
-namespace Content.Shared.Schematics;
+namespace Content.Shared.Construction;
 
 /// <summary>
-/// Used for starting a certain item crafting recipe through activating schematics
+/// Used for starting a certain item crafting recipe when activated.
 /// </summary>
 [RegisterComponent]
-public sealed partial class SchematicComponent : Component
+public sealed partial class StartItemConstructionOnActivateComponent : Component
 {
     [DataField("prototype", customTypeSerializer: typeof(PrototypeIdSerializer<ConstructionPrototype>))]
     public string? Prototype { get; private set; }
