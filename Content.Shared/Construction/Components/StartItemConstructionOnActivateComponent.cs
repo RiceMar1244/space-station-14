1b948e25a4487dce452e5cc7ae90fe92e2a5bc74
Brandon Metz <bmetz02@gmail.com>
2023-10-31 15:50:54 -0400

    Adds StartItemConstructionOnActivateSystem and StartItemConstructionOnActivateComponent

---
+++ Content.Shared/Construction/Components/StartItemConstructionOnActivateComponent.cs
@@ -0,0 +1,14 @@
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+using Content.Shared.Construction.Prototypes;
+
+namespace Content.Shared.Construction;
+
+/// <summary>
+/// Used by <see cref="StartItemConstructionOnActivateSystem"/> for starting a certain item crafting recipe on entity activation.
+/// </summary>
+[RegisterComponent]
+public sealed partial class StartItemConstructionOnActivateComponent : Component
+{
+    [DataField("prototype", customTypeSerializer: typeof(PrototypeIdSerializer<ConstructionPrototype>))]
+    public string? Prototype { get; private set; }
+}
